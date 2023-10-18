5d3c1b27aa2ce0f244f4eff35df3bb9f0422cd38
CanizalesJose <jose1605m@gmail.com>
2023-10-18 11:44:22 -0700

    Creado el repositorio

---
+++ Ejercicios.py
@@ -0,0 +1,12 @@
+a = 2 + 3
+b = a + 3.5
+
+print(a+b)
+
+mensaje = "Hola mundo"
+print(mensaje + " desde Python")
+
+print("La longitud del mensaje es: "+str(len(mensaje)))
+#str() convierte en string lo que reciba como argumento
+#len() recibe una lista como argumento y regresa la longitud
+
